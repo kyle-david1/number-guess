e4d5cebb7499562a1053bd374525db8d5501d30d
kyle-david1 <kdbaca5892@gmail.com>
2023-02-02 19:07:46 -0600

    JS functionality

--- script.js
+++ script.js
@@ -9,13 +9,25 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const number = Math.random()
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if(!guess) {
-    document.querySelector('.message').textContent = 'No Number! 🚫'
+  document.querySelector('.message').textContent = '🎉 No Number!'
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too High';
+    score --;
+    document.querySelector('.score').textContent = score; 
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too Low';
+    score --;
+    document.querySelector('.score').textContent = score; 
   }
 })
