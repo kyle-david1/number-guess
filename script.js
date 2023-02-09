454f3398d487668bc697af06062a816171b70a9c
kyle-david1 <kdbaca5892@gmail.com>
2023-02-08 19:14:03 -0600

    additional gameplay scenarios

--- script.js
+++ script.js
@@ -16,15 +16,27 @@ document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
-
+  
+// No input present
   if(!guess) {
   document.querySelector('.message').textContent = '🎉 No Number!'
+
+// When player wins 
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number';
+
+// When guess is too high    
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too High';
+    if (score > 0) {
+      document.querySelector('.message').textContent = '📈 Too High';
     score --;
     document.querySelector('.score').textContent = score; 
+    } else {
+      document.querySelector('.message').textContent = ' You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+// When guess is too low    
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = '📉 Too Low';
     score --;
